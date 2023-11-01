a3222eaeb762ddd395663fe4f45fef24704c5f9a
Caren-Koroeny <carensiya5@gmail.com>
2023-11-01 11:34:53 +0300

    use pop() method to remove the last element in an array

---
+++ js-dsa-freecodecamp/pop.js
@@ -0,0 +1,11 @@
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+const removedFromMyArray = myArray.pop();
+
+console.log(myArray);
+console.log(removedFromMyArray);
+
+// .pop() removes the last element from an array and returns that element.
+// Use the .pop() function to remove the last item from myArray and assign the popped off value to a new variable, removedFromMyArray.
