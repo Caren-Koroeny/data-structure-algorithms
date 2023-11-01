20504126aaecfbdd24b16545a9b3cbaa7c55f316
Caren-Koroeny <carensiya5@gmail.com>
2023-11-01 11:41:53 +0300

    used shift to remove the first item of an element

---
+++ shift.js
@@ -0,0 +1,7 @@
+// shift() method removes the first element of an array.
+// Use the .shift() function to remove the first item from myArray and assign the "shifted off" value to a new variable, removedFromMyArray.
+const myArray = [["John", 23], ["dog", 3]];
+
+const removedFromMyArray = myArray.shift();
+console.log(removedFromMyArray)
+console.log(myArray)
