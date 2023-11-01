e0e68ab775fffc420197bbb1f73fda34b3ce4171
Caren-Koroeny <carensiya5@gmail.com>
2023-11-01 11:45:29 +0300

    used unshift method to add element at the begining of an array.

---
+++ unshift.js
@@ -0,0 +1,4 @@
+// .unshift() works exactly like .push(), but instead of adding the element at the end of the array, unshift() adds the element at the beginning of the array.
+const myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+myArray.unshift(["Paul", 35]);
