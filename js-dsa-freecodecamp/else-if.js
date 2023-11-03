57948087023064fac2c5409bd3e10a306202e550
Caren-Koroeny <carensiya5@gmail.com>
2023-11-03 10:12:25 +0300

    Test else if statement

---
+++ js-dsa-freecodecamp/else-if.js
@@ -0,0 +1,11 @@
+function testeElseIf(val){
+    if(val > 10){
+        return "Greater than 10";
+    }else if(val < 5){
+        return "Smaller than 5";
+    } else {
+        return "Between 5 and 10";
+    }
+}
+
+testeElseIf(7);
