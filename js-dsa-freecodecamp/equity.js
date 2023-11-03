ca67e31e6306c28626e7fd4f301a755981c63a2b
Caren-Koroeny <carensiya5@gmail.com>
2023-11-03 08:13:40 +0300

    Added greater then operator

---
+++ js-dsa-freecodecamp/equity.js
@@ -0,0 +1,10 @@
+// The equality operator compares two values and returns true if they're equivalent or false if they are not.
+// Setup
+function testEqual(val) {
+    if (val == 12) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  testEqual(10);
