5f6f8ae581f40f51c630e48759b4bd42720b1f4d
Caren-Koroeny <carensiya5@gmail.com>
2024-02-12 04:26:22 +0300

    Using object to organize the code

---
+++ odin-js/4.1 object_design_pattern.js
@@ -0,0 +1,16 @@
+// One of the simplest ways you can begin to organize your code is by grouping things into objects.
+const playerOneName = "tim";
+const playerTwoName = "jenn";
+const playerOneMaker = "X";
+const playerTwoMaker = "O";
+
+// EXAMPLE ONE
+const playerOne = {
+    name: "tim",
+    marker: "X"
+};
+
+const playerTwo = {
+    name: "jenn",
+    marker: "O"
+};
